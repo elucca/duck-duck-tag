0a9da8a18c869a5f114e07e0ab3b42f2e494dbf7
toppyy <topias.pyykkonen@helsinki.fi>
2020-06-01 07:15:45 +0300

    creates a simple test for Image-component

---
+++ test/components/Image.spec.tsx
@@ -0,0 +1,28 @@
+/* eslint react/jsx-props-no-spreading: off */
+
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Image from '../../app/components/Image';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+
+  const component = shallow(<Image/>);
+  return {
+    component,
+    buttons: component.find('button')
+  };
+}
+
+describe('Image component', () => {
+
+  it('should have a button', () => {
+    const { buttons } = setup();
+    
+    expect(buttons.length>0).toBe(true);
+  });
+
+  
+});
