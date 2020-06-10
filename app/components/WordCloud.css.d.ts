af492aaa54ac1f0bf4e181bf31768e0ea0aa8e90
perander <silva.perander@helsinki.fi>
2020-06-10 12:34:40 +0300

    generate word clouds per service per job

---
+++ app/components/WordCloud.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "rows": string;
+  readonly "row": string;
+};
+export = styles;
+
---
+++ app/components/WordCloud.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactWordcloud from 'react-wordcloud'
+import styles from './WordCloud.css'
+
+const WordCloud = ({ job, animation }) => {
+
+    const services = job.services
+    const result = job.result
+
+    if (!result || result.length === 0 || animation) {
+        return ''
+    }
+
+   
+    const options = {
+        fontSizes: [20, 45], //min, max (default min 5 is barely visible)
+        rotations: 0 //all words horizontally
+         //TODO: maybe exclude blue colors for now (background is blue)
+    }
+
+
+    const tags = (service) => {
+        return result.filter(row => row.service === service).map(row =>
+            ({
+                text: row.label,
+                // TODO: value could be the word count (somehow combined with accuracies ?)
+                value: row.accuracy
+            })
+        )
+    }
+
+    // TODO/warning: why does my computer crash if i don't include the size style below
+    return (
+        <div className={styles.rows} style={{ height: 300, width: 1000 }}>
+            {
+                services.map(service => {
+                    return(
+                        <div className={styles.row}>
+                            <ReactWordcloud options={options}
+                            words={tags(service)} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default WordCloud
